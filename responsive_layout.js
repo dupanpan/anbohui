7bd8f828b1c0c8c3805e53e40bdfb2c91c1dc2a3
Adele_Du <toughhoneybear@163.com>
2020-05-26 09:46:07 +0800

    header

--- responsive_layout.js
+++ responsive_layout.js
@@ -2,3 +2,10 @@ var viewport = document.documentElement;
 var width = viewport.clientWidth;
 var fontSize = width / 1920 * 16;
 viewport.style.fontSize = fontSize + "px";
+
+window.onresize = function() {
+    var viewport = document.documentElement;
+    var width = viewport.clientWidth;
+    var fontSize = width / 1920 * 16;
+    viewport.style.fontSize = fontSize + "px";
+}
