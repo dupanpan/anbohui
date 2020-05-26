47de5c601d748d263489ff22ecc9ec2ef95f3496
Adele_Du <toughhoneybear@163.com>
2020-05-26 10:17:34 +0800

    check viewport min-width

--- responsive_layout.js
+++ responsive_layout.js
@@ -1,11 +1,11 @@
 var viewport = document.documentElement;
-var width = viewport.clientWidth;
+var width = viewport.clientWidth < 1024 ? 1024 : viewport.clientWidth;
 var fontSize = width / 1920 * 16;
 viewport.style.fontSize = fontSize + "px";
 
 window.onresize = function() {
     var viewport = document.documentElement;
-    var width = viewport.clientWidth;
+    var width = viewport.clientWidth < 1024 ? 1024 : viewport.clientWidth;
     var fontSize = width / 1920 * 16;
     viewport.style.fontSize = fontSize + "px";
 }
